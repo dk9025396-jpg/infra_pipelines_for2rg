9e17d9d7650f3f1a3753582d570bf617ad203301
dk9025396 <dk9025396@gmailcom>
2026-07-30 02:13:48 +0530

    Added main.tf

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+    required_providers{
+        azurerm = {
+            source="hashicorp/azurerm"
+            version = "4.80.0"
+        }
+    }
+}
+provider "azurerm"{
+    features{}
+}
+resource"azurerm_resouce_group" "rg3" {
+    name = "mani"
+    location ="centralindia"
+}
+resource"azurerm_resouce_group" "rg4" {
+    name = "sujju"
+    location ="centralindia"
+}
